d5aaf1e492807581ea857935af597845bfddcd50
Alexey Novakov <novakov.alex@gmail.com>
2021-03-28 21:50:59 +0200

    refactor toString

--- src/main/scala/ml/tensors/tensor.scala
+++ src/main/scala/ml/tensors/tensor.scala
@@ -5,12 +5,16 @@ import scala.reflect.ClassTag
 import math.Ordering.Implicits.infixOrderingOps
 import Tensor._
 
-sealed trait Tensor[T]:  
+sealed trait Tensor[T]:
+
   def shape: List[Int]
-  def length: Int = 
-    shape.headOption.getOrElse(0)
-  def shape(axis: Int): List[Int] = 
-    shape.drop(axis)
+
+  def length: Int = shape.headOption.getOrElse(0)
+
+  def shape(axis: Int): List[Int] = shape.drop(axis)
+
+  def meta[T: ClassTag] = 
+    s"shape: ${shape.mkString("x")}, ${getClass.getSimpleName}[${summon[ClassTag[T]]}]"
 
 object Tensor:    
   def printArray(a: Array[_], meta: String): String = 
@@ -23,7 +27,7 @@ object Tensor:
             val end = if body.last == ']' then s"\n${" " * level}]" else "]"
             s"$start$body$end"
           case _ => s"$e"
-      }
+      }    
     val str = loop(a).mkString(", ")  
     s"$meta:\n[" + str + (if str.last == ']' then "\n" else "") + "]"  
 
@@ -37,8 +41,6 @@ case class Tensor0D[T: ClassTag](data: T) extends Tensor[T]:
   override val length: Int = 1
   
   override val shape: List[Int] = length :: Nil
-  
-  private val meta = s"shape: $length, Tensor0D[${summon[ClassTag[T]]}]"
 
   override def toString: String =
     s"$meta:\n" + data + "\n"  
@@ -46,9 +48,8 @@ case class Tensor0D[T: ClassTag](data: T) extends Tensor[T]:
 case class Tensor1D[T: ClassTag](data: Array[T]) extends Tensor[T]:
   override def shape: List[Int] = List(data.length)
 
-  override def toString: String =
-    val meta = s"shape: ${shape.head}, Tensor1D[${summon[ClassTag[T]]}]"
-    s"$meta:\n[" + data.mkString(",") + "]\n"
+  override def toString: String =    
+    printArray(data, meta)
 
   override def length: Int = data.length
 
@@ -67,9 +68,7 @@ case class Tensor2D[T: ClassTag](data: Array[Array[T]]) extends Tensor[T]:
     s"shape: ${shape.mkString("x")}, Tensor2D[${summon[ClassTag[T]]}]"
 
   override def toString: String =
-    s"$meta:\n[" + data
-      .map(_.mkString("[", ",", "]"))
-      .mkString("\n ") + "]\n"  
+    printArray(data, meta)    
 
   override def length: Int = data.length
 
@@ -91,16 +90,12 @@ case class Tensor3D[T: ClassTag](data: Array[Array[Array[T]]]) extends Tensor[T]
 
   override def length: Int = data.length
 
-  // TODO: remove duplication
-  private val meta =
-    s"shape: ${shape.mkString("x")}, Tensor3D[${summon[ClassTag[T]]}]"
-
   override def toString: String =    
     printArray(data, meta)
 
 object Tensor3D:
   def apply[T: ClassTag](matrices: Tensor2D[T]*): Tensor3D[T] =
-    Tensor3D(matrices.toArray.map(t => t.data))
+    Tensor3D(matrices.toArray.map(_.data))
 
 case class Tensor4D[T: ClassTag](data: Array[Array[Array[Array[T]]]]) extends Tensor[T]:
   def shape4D: (Int, Int, Int, Int) =
@@ -118,9 +113,6 @@ case class Tensor4D[T: ClassTag](data: Array[Array[Array[Array[T]]]]) extends Te
     shape4D.toList    
 
   override def length: Int = data.length
-
-  private val meta =
-    s"shape: ${shape.mkString("x")}, Tensor4D[${summon[ClassTag[T]]}]"
 
   override def toString: String =    
     printArray(data, meta)
